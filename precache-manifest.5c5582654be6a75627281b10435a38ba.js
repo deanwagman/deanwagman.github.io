a16bf1cfb00228e6ec57bf878dae063bd0b321c7
Dean Wagman <deanwagman@deans-macbook.lan>
2019-10-07 22:49:51 -0700

    Updates

---
+++ precache-manifest.5c5582654be6a75627281b10435a38ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba70d403edaa432e7e5564172b0125bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79dc67ab7027abb194aa",
+    "url": "/static/css/main.64de09a2.chunk.css"
+  },
+  {
+    "revision": "c0724f98abf93c9fb6b1",
+    "url": "/static/js/2.24b21063.chunk.js"
+  },
+  {
+    "revision": "79dc67ab7027abb194aa",
+    "url": "/static/js/main.d27309a5.chunk.js"
+  },
+  {
+    "revision": "4ecb1af9d6c92bdf1f10",
+    "url": "/static/js/runtime-main.f5f1ea12.js"
+  }
+]);
